4eab511f707af540445d7bc4c2b5b8f9f65ffea9
davi carvalho <davi.carvalho@redemaisvoce.com.br>
2018-09-16 14:26:32 -0300

    Suíte de teste

---
+++ CursoSelenium/CursoSelenium/src/test/java/SuiteTeste.java
@@ -0,0 +1,25 @@
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author davicfg
+ */
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    TesteCadastro.class,
+    TesteRegrasCadastro.class,
+    CampoTreinamentoTest.class
+})
+public class SuiteTeste {
+    
+}
