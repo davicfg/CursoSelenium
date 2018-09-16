360029bab33743472ddbd0d2341141ae3188a9da
davi carvalho <davi.carvalho@redemaisvoce.com.br>
2018-09-16 14:33:35 -0300

    Removendo teste que estão na parte do TesteRegra

--- CursoSelenium/CursoSelenium/src/test/java/TesteCadastro.java
+++ CursoSelenium/CursoSelenium/src/test/java/TesteCadastro.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class TesteCadastro {
 
     private WebDriver driver;
-    private DSL dsl;
     private CampoTreinamentoPage page;
 
     @Before
@@ -25,7 +24,6 @@ public class TesteCadastro {
         driver = new FirefoxDriver();
         driver.manage().window().setSize(new Dimension(1200, 765));
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/recursos/componentes.html");
-        dsl = new DSL(driver);
         page = new CampoTreinamentoPage(driver);
     }
 
@@ -57,48 +55,5 @@ public class TesteCadastro {
         Assert.assertEquals("Comida: Pizza", page.obterComidaCadastro());
         Assert.assertEquals("Escolaridade: mestrado", page.obterEscolaridadeCadastro());
         Assert.assertEquals("Esportes: Natacao", page.obterEsporteCadastro());
-    }
-
-    @Test
-    public void deveValidarNomeObrigatorio() {
-        page.cadastrar();
-        Assert.assertEquals("Nome eh obrigatorio", dsl.alertaObterTextoEAceita());
-    }
-
-    @Test
-    public void deveValidarSobrenomeObrigatorio() {
-        page.setNome("Davi");
-        page.cadastrar();
-        Assert.assertEquals("Sobrenome eh obrigatorio", dsl.alertaObterTextoEAceita());
-    }
-
-    @Test
-    public void deveValidarSexoObrigatorio() {
-        page.setNome("Nome qualquer");
-        page.setSobrenome("Sobrenome qualquer");
-        page.cadastrar();
-        Assert.assertEquals("Sexo eh obrigatorio", dsl.alertaObterTextoEAceita());
-    }
-
-    @Test
-    public void deveValidarComidaVegetariana() {
-        page.setNome("Nome qualquer");
-        page.setSobrenome("Sobrenome qualquer");
-        page.setSexoMasculino();
-        page.setComidaCarne();
-        page.setComidaVegetariano();
-        page.cadastrar();
-        Assert.assertEquals("Tem certeza que voce eh vegetariano?", dsl.alertaObterTextoEAceita());
-    }
-
-    @Test
-    public void deveValidarEsportistaIndeciso() {
-        page.setNome("Nome qualquer");
-        page.setSobrenome("Sobrenome qualquer");
-        page.setSexoMasculino();
-        page.setComidaPizza();
-        page.setEsporte("Karate", "O que eh esporte?");
-        page.cadastrar(); 
-        Assert.assertEquals("Voce faz esporte ou nao?", dsl.alertaObterTextoEAceita());
     }
 }
